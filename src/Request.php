5b779137e6d811860a4987c8a20c2bb31aa8080f
Frederick Montiel <86737117+FrederickMontiel@users.noreply.github.com>
2022-10-30 00:07:33 -0600

    The files are ordered
    [
        {
            "imagenes" : {
                "name" : ["imagen1", "imagen2", "imagen3"],
                "url" : ["http://www.imagen1.com", "http://www.imagen2.com", "http://www.imagen3.com"],
                "size" : ["100x100", "200x200", "300x300"]
            }
        },
        
        {
            "imagenes" : {
                "0" : {
                    "name" : "imagen1",
                    "url" : "http://www.imagen1.com",
                    "size" : "100x100"
                },
                "1" : {
                    "name" : "imagen2",
                    "url" : "http://www.imagen2.com",
                    "size" : "200x200"
                },
                "2" : {
                    "name" : "imagen3",
                    "url" : "http://www.imagen3.com",
                    "size" : "300x300"
                }
            }
        }    
    ]

--- src/Request.php
+++ src/Request.php
@@ -12,8 +12,23 @@
             //$this->params = json_decode(json_encode($this->decodeUrlData($params)));
             $this->params = json_decode(json_encode($params));
             $this->body = json_decode(json_encode($body));
-            $this->files = json_decode(json_encode($_FILES));
+            $this->files = self::orderFiles(json_decode(json_encode($_FILES)));
             $this->headers = json_decode(json_encode($headers));
+        }
+        
+        public static function orderFiles($files){
+            foreach ($files as $keyField => $valueField) {
+                $arrayNuevo = [];
+    
+                foreach ($valueField as $keyAttribute => $valueAttribute) {
+                    foreach ($valueAttribute as $key => $value) {
+                        $arrayNuevo[$key][$keyAttribute] = $value;
+                    }
+                }
+    
+                $files->$keyField = $arrayNuevo;
+            }
+            return $files;
         }
 
         /*private function decodeUrlData($params){
